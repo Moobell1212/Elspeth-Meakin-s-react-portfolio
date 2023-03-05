a8a04b1a1cdceaf1f9fe37625e797d794aae0b25
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-05 19:36:17 +0000

    navbar file and index added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      
+      <Navbar />
     </Router>
   );
 }
