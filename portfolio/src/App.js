8eed05153cc7fbd2349269c0af668a75104bc18c
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-05 20:38:27 +0000

    draft navbar added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navibar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <div>
+      <Navibar />
+      </div>
     </Router>
   );
 }
---
+++ portfolio/src/components/Navbar/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import { Link, NavLink } from 'react-router-dom';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './style.css';
+
+function Navibar() {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="light">
+      <Container>
+        <Navbar.Brand href="#home">Elspeth Meakin's Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home-oage">Home page</Nav.Link>
+            <Nav.Link href="#project">Projects</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#contact">
+              Contact Me
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    );
+}
+
+export default Navibar;
