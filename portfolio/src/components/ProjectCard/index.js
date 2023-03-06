f55882662289887a693a3d001b40497a964bb6cc
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-06 12:47:43 +0000

    all cards shown using projects file

--- portfolio/src/components/ProjectCard/index.js
+++ portfolio/src/components/ProjectCard/index.js
@@ -1,1 +1,17 @@
 import React from "react";
+
+function ProjectCard(props) {
+    return (
+        <div class="card border-dark text-center">
+            <img src={props.image} class="card-img-top" alt={props.name} />
+                <div class="card-body">
+                    <h5 class="card-title">{props.name}</h5>
+                    <p class="card-text">{props.info}</p>
+                    <a href={props.site} target="_blank" class="btn btn-primary">Website</a>
+                    <a href={props.github} target="_blank" class="btn btn-primary">Github</a>
+                </div>
+        </div>
+    );
+}
+
+export default ProjectCard;
--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
+import ProjectCard from '../components/ProjectCard';
+import ProjectList from '../projects.json'
 
 function Projects() {
     return (
         <div>
             <Container>
                 <h1>Here are my past projects:</h1>
+                {ProjectList.map(f => <ProjectCard key={f.id}
+                    id={f.id}
+                    name={f.name}
+                    image={f.image}
+                    gif={f.gif}
+                    site={f.site}
+                    github={f.github}
+                    info={f.info}
+                     />)}
             </Container>
         </div>
     )
