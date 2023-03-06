dd708bf4fe57b6281c1174bc4813e9c0bc5b8bed
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-06 13:39:24 +0000

    project cards layot fully fixed

--- portfolio/src/components/ProjectCard/index.js
+++ portfolio/src/components/ProjectCard/index.js
@@ -5,11 +5,11 @@ function ProjectCard(props) {
     return (
         <div class="card border-dark text-center">
             <img src={props.image} class="card-img-top" alt={props.name} />
-                <div class="card-body">
+                <div class="card-body w-40">
                     <h5 class="card-title">{props.name}</h5>
                     <p class="card-text">{props.info}</p>
-                    <a href={props.site} target="_blank" class="btn btn-primary">Website</a>
-                    <a href={props.github} target="_blank" class="btn btn-primary">Github</a>
+                    <a href={props.site} rel="noreferrer" target="_blank" class="btn btn-primary">Website</a>
+                    <a href={props.github} rel="noreferrer" target="_blank" class="btn btn-primary">Github</a>
                 </div>
         </div>
     );
