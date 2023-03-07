4e41ce760badfdfb9491bf7264206762e5fa3121
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-07 14:03:30 +0000

    draft CV file and click download added

--- portfolio/src/pages/contact.js
+++ portfolio/src/pages/contact.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Container from 'react-bootstrap/Container';
+import Avatar from "../images/1096_generated.jpg";
+import CV from "../downloads/Untitled.pdf"
 
 function Contact(){
     return (
@@ -7,9 +9,10 @@ function Contact(){
         <Container>
         <h1>Contact me!</h1>
         <h5>Email: <a href = "mailto: myemail@hotmail.co.uk">myemail@hotmail.co.uk</a></h5>
-        <h5>My <a href="myCV">CV</a></h5>
         <h5>GitHub: <a href="https://github.com/Moobell1212" target="_blank">Moobell1212</a></h5>
         <h5>LinkedIn: <a href="https://www.linkedin.com/in/elspeth-meakin-7289b3126/" target="_blank">Elspeth Meakin</a></h5>
+        <h5>My <a href="myCV">CV</a></h5>
+        <a href={CV} download><img src={Avatar} alt="Picture CV" width="500px"/></a>
         </Container>
         </div>
     );
