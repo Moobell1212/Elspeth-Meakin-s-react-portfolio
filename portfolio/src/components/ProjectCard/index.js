e92389fead0f1991c8705a331009c4073954475f
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-07 21:18:33 +0000

    initial code written for if the card should have an image or gif

--- portfolio/src/components/ProjectCard/index.js
+++ portfolio/src/components/ProjectCard/index.js
@@ -4,11 +4,16 @@ import "./style.css";
 function ProjectCard(props) {
     return (
         <div class="card border-dark text-center">
-            <img src={props.image} class="card-img-top" alt={props.name} />
-                <div class="card-body w-40">
-                    <h5 class="card-title">{props.name}</h5>
-                    <p class="card-text">{props.info}</p>
-                    {!props.site ? (
+            {!props.gif ? (
+                <img src={props.image} class="card-img-top" alt={props.name} />
+            ) : (
+                <iframe src={props.gif} alt={props.name} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            )
+            }
+            <div class="card-body w-40">
+                <h5 class="card-title">{props.name}</h5>
+                <p class="card-text">{props.info}</p>
+                {!props.site ? (
                     <a href={props.github} rel="noreferrer" target="_blank" className="btn btn-primary">Github</a>
                 ) : (
                     <div><a href={props.github} rel="noreferrer" target="_blank" className="btn btn-primary">Github</a>
--- portfolio/src/pages/contact.js
+++ portfolio/src/pages/contact.js
@@ -11,7 +11,7 @@ function Contact(){
         <h5>Email: <a href = "mailto: myemail@hotmail.co.uk">myemail@hotmail.co.uk</a></h5>
         <h5>GitHub: <a href="https://github.com/Moobell1212" target="_blank">Moobell1212</a></h5>
         <h5>LinkedIn: <a href="https://www.linkedin.com/in/elspeth-meakin-7289b3126/" target="_blank">Elspeth Meakin</a></h5>
-        <h5>My <a href="myCV">CV</a></h5>
+        <h5>My CV:</h5>
         <a href={CV} download><img src={Avatar} alt="Picture CV" width="500px"/></a>
         </Container>
         </div>
