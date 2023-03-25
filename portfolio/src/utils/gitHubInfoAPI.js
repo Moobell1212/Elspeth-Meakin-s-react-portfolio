f4758b1ebcd316b8b25fd587009bcc03eeff2540
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-25 16:03:51 +0000

    gaps in navigation bar fixed

--- portfolio/src/utils/gitHubInfoAPI.js
+++ portfolio/src/utils/gitHubInfoAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const bioApiUrl = "https://api.github.com/users/Moobell1212";
 
-export default function GitHubBioInfo() {
+export default function GitHubInfo() {
 
     return (
         axios.get(bioApiUrl)
