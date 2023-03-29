dce5eec3048a8ea8162fd39852cf99d558281df6
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-29 18:48:42 +0100

    layout of banner fixed due to message addition

--- portfolio/src/components/HomepageBanner/bannerMessage.js
+++ portfolio/src/components/HomepageBanner/bannerMessage.js
@@ -4,17 +4,17 @@ const message = "Hello, my name is";
 
 export default function bannerMessage() {
     const Typer = ({ speed , children }) => {
-        const [idx, setidx] = useState(0);
+        const [msg, setmsg] = useState(0);
         useEffect(() => {
-            const timer = window.setInterval(() => setidx((v) => v + 1), speed);
+            const timer = window.setInterval(() => setmsg((v) => v + 1), speed);
             return () => window.clearInterval(timer);
         });
 
-        return <div>{children.substr(0, idx)}</div>;
+        return <div>{children.substr(0, msg)}</div>;
     };
     return (
         <div>
-            <Typer speed={250} children={message}></Typer>
+            <Typer speed={250} children={message} ></Typer>
         </div>
     );
 };
