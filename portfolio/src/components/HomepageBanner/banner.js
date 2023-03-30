dfc752820d403f0a7f54d86bf43e45031f3cebad
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-30 20:09:07 +0100

    banner layout altered

--- portfolio/src/components/HomepageBanner/banner.js
+++ portfolio/src/components/HomepageBanner/banner.js
@@ -15,16 +15,16 @@ export default function HomepageBanner() {
     return (
         <div className="avatarbanner" style={{ backgroundImage: `url(${BannerBackground})`, backgroundSize: "100%", position: "relative", height: "calc(100vh - 80px)" }}>
             <Container id="avatarbannercontent" style={{ display: "flex", flexDirection: "column", justifyContent: "center", height: "100%" }}>
-                <div>
-                    <BannerMessage style={{ display: "block" }} />
                     <div style={{ display: "flex", flexWrap: "wrap"}} >
-                        <div style={{ display: "inline", margin: "auto 0px" }}>
+                        <div style={{ display: "inline" }}>
+                        <BannerMessage style={{ display: "block" }} />
+                        <div style={{ height: 250, position: "relative", minWidth: 650 }}>
                             <h1 style={{ }}>Elspeth Meakin</h1>
-                            <h2 style={{ }}>Welcome to my portfolio website!</h2>
+                            <h2 style={{ position: "absolute", bottom:0}}>Welcome to my portfolio website!</h2>
+                        </div>
                         </div>
                         <img src={profilePic} className="img-responsive" alt="Elspeth Meakin" style={{ margin: "auto", width: 300, boxShadow: "0 3px 6px #000, 0 3px 6px #000", borderRadius: "50%", alignContent: "center" }} />
                     </div>
-                </div>
                 <div class="bannercaption" style={{ fontSize: 8, position: "absolute", bottom: 10, left: 10 }}><a href="https://www.vecteezy.com/free-vector/banner-background" target="blank" >Banner Background Vectors by Vecteezy</a></div>
             </Container>
         </div>
