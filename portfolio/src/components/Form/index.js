151fb4e6868be7a0c83b94bb0b8bdbbe941f3767
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-03-31 21:25:39 +0100

    contact page form removed

--- portfolio/src/components/Form/index.js
+++ portfolio/src/components/Form/index.js
@@ -3,8 +3,31 @@ import Form from 'react-bootstrap/Form';
 import "./style.css";
 
 export default function Contactform() {
+
+  const ContactForm = () => {
+
+    const [submitted, setSubmitted] = useState(false);
+    const handleSubmit = () => {
+      setTimeout(() => {
+        setSubmitted(true);
+      }, 100);
+    };
+
+    if (submitted) {
+      return (
+        <>
+          <h2>Thank you!</h2>
+          <div>We'll be in touch soon.</div>
+        </>
+      );
+    }
+  }
+
   return (
-    <Form>
+    <Form action={FORM_ENDPOINT}
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank">
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" id="email" required />
@@ -12,14 +35,13 @@ export default function Contactform() {
 
       <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label>Your Name</Form.Label>
-        <Form.Control type="text" placeholder="Your name" id="name" required/>
+        <Form.Control type="text" placeholder="Your name" id="name" required />
       </Form.Group>
 
       <Form.Group>
-      <Form.Label>Your Message</Form.Label>
-        <Form.Control as="textarea" placeholder="Your message" id="message" required/>
+        <Form.Label>Your Message</Form.Label>
+        <Form.Control as="textarea" placeholder="Your message" id="message" required />
       </Form.Group>
-
       <Button variant="info" type="submit" id="button">
         Submit
       </Button>
--- portfolio/src/pages/contact.js
+++ portfolio/src/pages/contact.js
@@ -1,21 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from 'react-bootstrap/Container';
 import myCV from "../images/myCV.png";
 import CV from "../downloads/Elspeth_Meakin's_CV.pdf";
-import Contactform from '../components/Form'; 
 
-export default function Contact(){
+export default function Contact() {
     return (
-        <div style={{ minHeight: "calc(100vh - 185px)", margin: "50px 0px"}}>
-        <Container>
-        <h1>Contact page</h1>
-        <h5>GitHub: <a href="https://github.com/Moobell1212" target="_blank" rel="noreferrer" >Moobell1212</a></h5>
-        <h5>LinkedIn: <a href="https://www.linkedin.com/in/elspeth-meakin-7289b3126/" target="_blank" rel="noreferrer" >Elspeth Meakin</a></h5>
-        <h2 style={{ marginTop: "20px" }}>Contact me via the form below:</h2>
-        <Contactform />
-        <h2>My CV:</h2>
-        <a href={CV} download><img src={myCV} alt="CV" width="100%" /></a>
-        </Container>
-        </div>
+            <Container style={{ minHeight: "calc(100vh - 240px)"}}>
+                <h1 style={{ marginTop: 50}}>Contact me via:</h1>
+                <h5 style={{ marginTop: 30}}>GitHub: <a href="https://github.com/Moobell1212" target="_blank" rel="noreferrer" >Moobell1212</a></h5>
+                <h5 style={{ marginTop: 10}}>LinkedIn: <a href="https://www.linkedin.com/in/elspeth-meakin/" target="_blank" rel="noreferrer" >Elspeth Meakin</a></h5>
+                <h2 style={{ margin: "50px 0px 20px 0px"}}>My CV:</h2>
+                <a href={CV} download ><img src={myCV} alt="CV" width="100%" style={{ marginBottom: 50}} /></a>
+            </Container>
     );
 }
