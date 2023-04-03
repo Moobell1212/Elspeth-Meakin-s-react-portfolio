c68bcdf89859fa2246446b78be67dc6b4bb81e93
Elspeth Meakin <elspeth_meakin@hotmail.co.uk>
2023-04-03 15:08:35 +0100

    project page subtitles change

--- portfolio/src/components/GitHubRepos/gitHubRepos.js
+++ portfolio/src/components/GitHubRepos/gitHubRepos.js
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import Container from 'react-bootstrap/Container';
 import RepoProjectCard from './repoProjectCard.js';
 import RepoAPI from '../../utils/repoAPI.js';
-import HighProjects from '../../utils/projects.json';
 import Button from "react-bootstrap/esm/Button.js";
+
+{/* import HighProjects from '../../utils/projects.json'; */ }
 
 export default function GitHubProjects() {
 
@@ -13,19 +14,16 @@ export default function GitHubProjects() {
         RepoAPI().then((response) => { setRepos(response.data) })
     }, []);
 
-    let highProjectTitles = [];
+    {/* let highProjectTitles = [];
     HighProjects.forEach(function (obj) {
         highProjectTitles.push(obj.name.split(" ").join("-"))
     });
 
-    // const removePortfolio = () => {
-    //     const removeTitle = (array) => {
-    //         array.filter(x => x.node_id === "R_kgDOJE8_rA")
-    //     }
-    //     setRepos(removeTitle(repos))
-    // }
-
-    // removePortfolio()
+    const removePortfolio = () => {
+        setRepos(current => {
+            return current.filter(repo => repo.name !== "Elspeth-Meakin-s-react-portfolio") })
+    }
+    */}
 
     const sortRepos = (sortBy) => {
         const sortFun = (a, b) => {
@@ -43,7 +41,7 @@ export default function GitHubProjects() {
 
     return (
         <Container>
-            <h2 style={{ margin: "50px 0px" }}>Here are my other repositories from Github:</h2>
+            <h2 style={{ margin: "50px 0px" }}>Here are my repositories from Github:</h2>
             <div className="sorting-buttons" style={{ marginBottom: 50 }}>
                 <Button onClick={() => sortRepos("created_at")} style={{ margin: "10px 10px" }}>Sort by Date Created</Button>
                 <Button onClick={() => sortRepos("updated_at")} style={{ margin: "10px 10px" }}>Sort by Date Updated</Button>
@@ -58,7 +56,7 @@ export default function GitHubProjects() {
                     github={f.svn_url}
                     created={f.created_at.slice(0, 10)}
                     pushed={f.pushed_at.slice(0, 10)}
-                    updated={f.created_at.slice(0,10)}
+                    updated={f.created_at.slice(0, 10)}
                 />)}
             </div>
         </Container>
